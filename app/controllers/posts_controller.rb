eb72c3a4de20df9a9f55d0779f06dd4d95dc40f4
Rosalie Quach <rosaliequach@gmx.net>
2022-05-30 16:52:34 +0200

    Created the new posts view

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,43 @@
+class PostsController < ApplicationController
+  before_action :post, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to post_path(@post)
+  end
+
+  def edit
+  end
+
+  def update
+    @post.update(post_params)
+    redirect_to post_path(@post)
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :description, :is_offer)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+end
