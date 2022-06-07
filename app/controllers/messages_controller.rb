e85efffafaad185c3ef17c786ec7cee4ad00600f
Rosalie <95964048+RosalieQuach@users.noreply.github.com>
2022-06-07 10:22:37 +0200

    Revert "Save chat progress"

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,13 +5,9 @@ class MessagesController < ApplicationController
     @message.chatroom = @chatroom
     @message.user = current_user
     if @message.save
-      ChatroomChannel.broadcast_to(
-        @chatroom,
-        render_to_string(partial: "message", locals: {message: @message})
-      )
-      head :ok
+      redirect_to chatroom_path(@chatroom, anchor: "message-#{@message.id}")
     else
-      render "chatrooms/show"
+      render 'chatrooms/show'
     end
   end
 end
